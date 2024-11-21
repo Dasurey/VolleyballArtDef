4f272d162e79c00e3d028a7674ccf37112191f25
Dasurey <dario.asurey@gmail.com>
2024-11-20 21:34:27 -0300

    Arreglar problemas de data-name, los px del navbar vertical y ponerle una animacion de carga de pagina

--- js/script.js
+++ js/script.js
@@ -1,103 +1,83 @@
-// Función para cargar el contenido del archivo HTML
-fetch('../archivo-general/head-content.html')
-    .then(response => response.text())
-    .then(data => {
-        headElement.innerHTML += data;
-    })
-    .catch(error => console.error('Error al cargar el contenido del header:', error));
-
-fetch('../archivo-general/header-content.html')
-    .then(response => response.text())
-    .then(data => {
-        headerElement.innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar el contenido del header:', error));
-
-fetch('../archivo-general/nav-primary-content.html')
-    .then(response => response.text())
-    .then(data => {
-        navPrimaryElement.innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar el contenido del nav-primary:', error));
-
-fetch('../archivo-general/nav-secondary-content.html')
-    .then(response => response.text())
-    .then(data => {
-        navSecondaryElement.innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar el contenido del nav-secondary:', error));
-
-fetch('../archivo-general/nav-secondary-second-content.html')
-    .then(response => response.text())
-    .then(data => {
-        navSecondarySecondElement.innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar el contenido del nav-secondary-second:', error));
-
-fetch('../archivo-general/footer-content.html')
-    .then(response => response.text())
-    .then(data => {
-        footerElement.innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar el contenido del footer:', error));
-
-// Función para cargar el archivo JSON ../lenguage/general/es.json
 document.addEventListener('DOMContentLoaded', () => {
     const loadingIndicator = document.getElementById('loading-indicator');
     loadingIndicator.style.display = 'block'; // Mostrar el indicador de carga
 
     // Función para cargar el archivo JSON y aplicar las traducciones
-    function applyTranslations() {
-        fetch('../lenguage/general/es.json')
-            .then(response => response.json())
-            .then(data => {
-                // Seleccionar todos los elementos que necesitan traducción
-                const elementsNameSection = document.querySelectorAll('[data-name]');
+    function applyTranslations(data) {
+        // Función para actualizar los textos de los elementos
+        function updateElements(attribute) {
+            const elementsNameSection = document.querySelectorAll(`[${attribute}]`);
+            console.log(`Elementos encontrados con ${attribute}:`, elementsNameSection.length);
 
-                // Actualizar los textos de los elementos
-                elementsNameSection.forEach(element => {
-                    const keysArray = element.getAttribute('data-name').split(',');
-                    keysArray.forEach(keysString => {
-                        const keys = keysString.split('.');
-                        let nameVariable = data;
-                        let position = null;
+            elementsNameSection.forEach(element => {
+                const keysArray = element.getAttribute(attribute).split(',');
+                keysArray.forEach(keysString => {
+                    const keys = keysString.split('.');
+                    let nameVariable = data;
+                    let position = null;
 
-                        // Verificar si la clave está en 'left' o 'right'
-                        if (keys[0] === 'left') {
-                            nameVariable = data.left;
-                            position = 'left';
-                        } else if (keys[0] === 'right') {
-                            nameVariable = data.right;
-                            position = 'right';
+                    // Verificar si la clave está en 'left' o 'right'
+                    if (keys[0] === 'left') {
+                        nameVariable = data.left;
+                        position = 'left';
+                    } else if (keys[0] === 'right') {
+                        nameVariable = data.right;
+                        position = 'right';
+                    }
+
+                    // Navegar por las claves del JSON
+                    keys.slice(1).forEach(key => {
+                        if (nameVariable && nameVariable[key] !== undefined) {
+                            nameVariable = nameVariable[key];
+                        } else {
+                            console.error(`Clave no encontrada: ${key}`);
+                            nameVariable = null;
                         }
+                    });
 
-                        // Navegar por las claves del JSON
-                        keys.slice(1).forEach(key => {
-                            if (nameVariable[key] !== undefined) {
-                                nameVariable = nameVariable[key];
-                            } else {
-                                console.error(`Clave no encontrada: ${key}`);
-                                nameVariable = null;
-                            }
-                        });
-
-                        // Aplicar la traducción en la posición correcta
-                        if (nameVariable) {
+                    // Aplicar la traducción en la posición correcta
+                    if (nameVariable) {
+                        if (Array.isArray(nameVariable)) {
+                            // Si es un array, recorrer los elementos y aplicar los cambios
+                            nameVariable.forEach(item => {
+                                if (item.href) {
+                                    element.href = item.href;
+                                }
+                                if (item.src) {
+                                    element.src = item.src;
+                                }
+                                if (item.alt) {
+                                    element.alt = item.alt;
+                                }
+                                if (item.text) {
+                                    if (position === 'left') {
+                                        element.innerHTML = `${item.text} ${element.innerHTML}`; // Añadir el texto a la izquierda del contenido existente
+                                    } else if (position === 'right') {
+                                        element.innerHTML += `${item.text}`; // Añadir el texto a la derecha del contenido existente
+                                    }
+                                }
+                            });
+                        } else {
+                            // Si no es un array, aplicar los cambios directamente
                             if (position === 'left') {
                                 element.innerHTML = `${nameVariable} ${element.innerHTML}`; // Añadir el texto a la izquierda del contenido existente
                             } else if (position === 'right') {
-                                element.innerHTML += ` ${nameVariable}`; // Añadir el texto a la derecha del contenido existente
+                                element.innerHTML += `${nameVariable}`; // Añadir el texto a la derecha del contenido existente
                             }
                         }
-                    });
+                        console.log('Texto aplicado:', nameVariable);
+                    } else {
+                        console.error(`No se pudo aplicar la traducción para: ${keysString}`);
+                    }
                 });
-
-                loadingIndicator.style.display = 'none'; // Ocultar el indicador de carga
-            })
-            .catch(error => {
-                console.error('Error al cargar las traducciones:', error);
-                loadingIndicator.style.display = 'none'; // Ocultar el indicador de carga en caso de error
             });
+        }
+
+        // Actualizar los elementos con data-name y data-name-inside
+        updateElements('data-name');
+        updateElements('data-name-inside');
+
+        loadingIndicator.style.display = 'none'; // Ocultar el indicador de carga
     }
 
     // Función para cargar el contenido del archivo HTML
@@ -105,11 +85,18 @@ document.addEventListener('DOMContentLoaded', () => {
         return fetch(url)
             .then(response => response.text())
             .then(data => {
-                element.innerHTML = data;
+                element.innerHTML += data;
             })
             .catch(error => console.error(`Error al cargar el contenido de ${url}:`, error));
     }
-    
+
+    // Función para cargar el archivo JSON
+    function loadJSON(url) {
+        return fetch(url)
+            .then(response => response.json())
+            .catch(error => console.error(`Error al cargar el archivo JSON: ${url}`, error));
+    }
+
     // Seleccionar los elementos que contienen el contenido de los archivos HTML
     const headElement = document.getElementById('headId');
     const headerElement = document.getElementById('headerId');
@@ -118,15 +105,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const navSecondarySecondElement = document.getElementById('nav-secondary-secondId');
     const footerElement = document.getElementById('footerId');
 
-    // Cargar el contenido de los archivos HTML y luego aplicar las traducciones
+    // Cargar el contenido de los archivos HTML y el archivo JSON, luego aplicar las traducciones
     Promise.all([
         loadHTMLContent('../archivo-general/head-content.html', headElement),
         loadHTMLContent('../archivo-general/header-content.html', headerElement),
         loadHTMLContent('../archivo-general/nav-primary-content.html', navPrimaryElement),
         loadHTMLContent('../archivo-general/nav-secondary-content.html', navSecondaryElement),
         loadHTMLContent('../archivo-general/nav-secondary-second-content.html', navSecondarySecondElement),
-        loadHTMLContent('../archivo-general/footer-content.html', footerElement)
-    ]).then(() => {
-        applyTranslations();
+        loadHTMLContent('../archivo-general/footer-content.html', footerElement),
+        loadJSON('../lenguage/general/es.json')
+    ]).then((results) => {
+        console.log('Contenido HTML y JSON cargado completamente');
+        const jsonData = results[6]; // El archivo JSON es el último en la lista de resultados
+        applyTranslations(jsonData);
     });
 });
